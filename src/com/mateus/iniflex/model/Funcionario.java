7271a3409f43b8758b0ff50522014dd8164e8638
Mateus <mateusoliveirdesouza@hotmail.com>
2024-06-12 03:54:31 -0300

    feat: create model Funcionario

---
+++ src/com/mateus/iniflex/model/Funcionario.java
@@ -0,0 +1,36 @@
+package com.mateus.iniflex.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+    
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+    
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, Salário: %s, Função: %s", super.toString(), salario, funcao);
+    }
+}
